ddb3d1a40fa5934782ad844094a246c57b0c82ab
Chibz9324 <chibz93@yahoo.co.uk>
2023-10-11 18:31:08 +0100

    Update Nominet.java

--- src/test/java/Nominet.java
+++ src/test/java/Nominet.java
@@ -6,11 +6,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Nominet {
 
 	public static void main(String[] args) {
+		//open Chrome
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		//Type URL
 		driver.get("https://www.nominet.uk/lookup/");
+		//Select lookup text box and type a domain name
 		driver.findElement(By.id("rdapLookup")).sendKeys("monster.wales");
-		driver.findElement(By.id("gtm-rdap-click")).click();
-		//^ Above navigates to the site to make a search query 
+		//Search
+		driver.findElement(By.id("gtm-rdap-click")).click(); 
 	}
 }
